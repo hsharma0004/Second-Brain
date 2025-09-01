6f1d2504229f2cbbcea069bcbd4fa6787d4668a3
hsharma0004 <harshmaheshsharma2004@gmail.com>
2025-09-01 22:56:55 +0530

    Middleware and content Schema

--- src/index.ts
+++ src/index.ts
@@ -4,8 +4,9 @@ import express from "express";
 import mongoose from "mongoose";
 import jwt from "jsonwebtoken";
 import { UserModel } from "./db.js";
-
-
+import { ContentModel } from "./db.js";
+import { JWT_PASSWORD } from "./config.js";
+import { UserMiddleware } from "./middleware.js";
 
 const app = express();
 app.use(express.json())
@@ -31,20 +32,69 @@ app.post("/api/v1/signup", async (req, res) => {
 
 })
 
-app.post("/api/v1/signin", (req, res) => {
+app.post("/api/v1/signin", async (req, res) => {
+  const username = req.body.username;
+  const password = req.body.password;
+  const existUser = await UserModel.findOne({
+    username,
+    password
+  })
+  if (existUser) {
+    const token = jwt.sign({
+      id: existUser._id
+    }, JWT_PASSWORD)
+
+    res.json({
+      token
+    })
+  }
+  else {
+    res.status(403).json({
+      message: "Incorrect Credentials"
+    })
+  }
+})
+
+app.post("/api/v1/content", UserMiddleware, async (req, res) => {
+  const link = req.body.link;
+  const type = req.body.type;
+  await ContentModel.create({
+    link,
+    type,
+    //title: req.body.title,
+    //@ts-ignore
+    userId: req.userId,
+    tags: []
+  })
+
+  return res.json({
+    message: 'Content added'
+  })
 
 })
 
-app.get("/api/v1/content", (req, res) => {
-
+app.get("/api/v1/content", UserMiddleware, async (req, res) => {
+  //@ts-ignore
+  const userId = req.userId;
+  const content = await ContentModel.find({
+    userId: userId
+  }).populate('userId')
+  res.json({
+    content
+  })
 })
 
-app.delete("/api/v1/content", (req, res) => {
+app.delete("/api/v1/content", async (req, res) => {
+  const contentId = req.body.contentId;
 
-})
-
-app.post("/api/v1/brain/share", (req, res) => {
-
+  await ContentModel.deleteMany({
+    contentId,
+    //@ts-ignore
+    userId: req.userId
+  })
+  res.json({
+    message: 'Deleted'
+  })
 })
 
 app.get("/api/v1/brain/:shareLink", (req, res) => {
